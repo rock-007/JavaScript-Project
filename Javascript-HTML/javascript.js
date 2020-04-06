d12331ddc9ba45d6f7957579329c4a4bc5cd8f78
rock-007 <59394025+rock-007@users.noreply.github.com>
2020-04-06 20:26:44 +0100

    commit

--- Javascript-HTML/javascript.js
+++ Javascript-HTML/javascript.js
@@ -8,7 +8,7 @@ let secondname = "Ashraf";
 let Fullname = firstname + '' +secondname;
 
 console.log(Fullname);
-console.log(Fullname[0]);
+console.log(Fullname[3]);
 console.log(Fullname.length);
 console.log(firstname.length);
 console.log(Fullname.toUpperCase());
@@ -17,6 +17,14 @@ let result= Fullname.toLowerCase();
 console.log(result,Fullname);
 
 const index = Fullname.indexOf('a');
+
 console.log(index);
+let result1 = Fullname.slice(4,5);
+console.log (result1);
+
+let result2 = Fullname.substr(4,5);
+console.log (result2);
 
 
+let result3 = Fullname.replace ('a','b');
+console.log (result3);
