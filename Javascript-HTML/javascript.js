4db642d386f148e97d4fa7e3d86454a77fa09874
umair ashraf <[umair.ashraf@commsworld.com]>
2020-04-07 20:29:54 +0100

    commit

--- Javascript-HTML/javascript.js
+++ Javascript-HTML/javascript.js
@@ -28,3 +28,83 @@ console.log (result2);
 
 let result3 = Fullname.replace ('a','b');
 console.log (result3);
+
+let radius = 10;
+    const pi = 3.14;
+    console.log(radius,pi);
+
+    console.log(10/2);
+    // remain is %
+    let result7 =radius % 3;
+    console.log(result7);
+
+    let result8 = pi * radius ** 2;
+    console.log(result8);
+
+    let like = 10;
+
+    like =like +1;
+    like++;
+    console.log(like);
+    like--;
+    console.log(like);
+
+    let likess = 20;
+    likess += 20;
+    console.log(likess);
+
+// NaN - not a number
+// 
+console.log(5/ 'hello');
+
+let result9= 'the blog has' + likess + likess;
+
+console.log(result9);
+
+
+const title = 'Mr';
+const auther ='ali';
+const likes = 30;
+
+//1-concatenation way
+let result10 = 'the blog is' + title + auther + 'of' + likes;
+console.log(result10);
+
+
+
+// 2-template string wat (back ticks )
+
+let results11 = `the log is ${title}${auther} of ${likes}`
+
+// 3-creating html templetates
+
+let html = `
+
+<h2> ${title} </h2>
+<p>by ${auther} </p>
+<span> This blog has ${likes} likes </span>
+
+
+
+`;
+console.log(html);
+
+let ninjas = ['shaun', 'umair','ali'];
+
+console.log(ninjas);
+console.log(ninjas[1]);
+
+let ages1 = [20,25,30,25];
+console.log(ages1[2]);
+
+let random = ['shaun', 'uzair', 30];
+
+console.log(random);
+
+
+
+
+
+
+
+
