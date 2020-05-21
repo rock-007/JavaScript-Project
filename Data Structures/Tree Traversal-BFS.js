b922450f0a50b9b717e28dbc596631fd0ff636d3
rock-007 <59394025+rock-007@users.noreply.github.com>
2020-05-21 11:41:34 +0100

    commit

--- Data Structures/Tree Traversal-BFS.js
+++ Data Structures/Tree Traversal-BFS.js
@@ -1,3 +1,18 @@
 
 // breath first search and depth first tree
-var 
+function BFS() {
+    var data =[];
+    var queue =[];
+  var  node = this.root;
+    queue.push(this.root);
+    while(queue.length){
+        node =queue.shift()
+        data.push(node);
+        if (node.left) queue.push(node.left)
+        if(node.right) queue.push(node.right)
+    }
+    return data;
+    }
+    
+    BFS()
+    
