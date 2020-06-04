6eec0033b609af2ac8c7b611e61e9819db539fd0
rock-007 <59394025+rock-007@users.noreply.github.com>
2020-06-04 17:29:54 +0100

    commit

--- Protfolio-updated/project2/main.js
+++ Protfolio-updated/project2/main.js
@@ -108,8 +108,8 @@ function fetchIssues() {
             '<h3>' + desc + '</h3>' +
             '<p><span class="glyphicon glyphicon-time"></span>' + severity + '</p>' +
             '<p><span class="glyphicon glyphicon-user"></span>' + assignedTo + '</p>' +
-            '<a href="#" onclick="setStatusClosed('${id}' )" class="btn btn-warning"> close</a>' +
-            '<a href="#" onclick="deleteIssue(\'' + id + '\')"class="btn btn-danger"> Delete</a>' +
+            '<a href="#" onclick="setStatusClosed(`${id}` )" class="btn btn-warning"> close</a>' +
+            '<a href="#" onclick="deleteIssue(`${id}`)"class="btn btn-danger"> Delete</a>' +
             '</div>';
     }
 
