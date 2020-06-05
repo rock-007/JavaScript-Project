4e68dd5789009f9d3bc204c999f185bfacbe19c5
rock-007 <59394025+rock-007@users.noreply.github.com>
2020-06-05 17:59:16 +0100

    commit

--- Protfolio-updated/project2/main.js
+++ Protfolio-updated/project2/main.js
@@ -102,15 +102,23 @@ function fetchIssues() {
         var severity = issues[i].severity;
         var assignedTo = issues[i].assignedTo;
         var status = issues[i].status;
-        issuesList.innerHTML += '<div class="well">' +
-            '<h6> Isssue ID' + id + '</h6>' +
-            '<p> <span class="label label-info">' + status + '</span></p>' +
-            '<h3>' + desc + '</h3>' +
-            '<p><span class="glyphicon glyphicon-time"></span>' + severity + '</p>' +
-            '<p><span class="glyphicon glyphicon-user"></span>' + assignedTo + '</p>' +
-            '<a href="#" onclick="setStatusClosed(`${id}` )" class="btn btn-warning"> close</a>' +
-            '<a href="#" onclick="deleteIssue(`${id}`)"class="btn btn-danger"> Delete</a>' +
-            '</div>';
+        issuesList.innerHTML += ` <div class="well"> 
+
+        <h6> Isssue ID  ${id}  </h6> 
+
+        <p> <span class="label label-info">  ${status} </span></p> 
+
+        <h3>  ${desc}  </h3> 
+
+        <p><span class="glyphicon glyphicon-time"></span> ${severity}  </p>
+
+        <p><span class="glyphicon glyphicon-user"></span>  ${assignedTo} </p> 
+
+        <a href="#" onclick="setStatusClosed('${id}')" class="btn btn-warning"> close</a> 
+
+        <a href="#" onclick="deleteIssue('${id}')"class="btn btn-danger"> Delete</a> 
+
+        </div> `;
     }
 
 }
