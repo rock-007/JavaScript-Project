e8ad53a99b65a57c980a5ead5b150fcd42e5bfb2
rock-007 <59394025+rock-007@users.noreply.github.com>
2020-06-06 16:20:32 +0100

    commit

--- Protfolio-updated/Project6/forcast-calculation.js
+++ Protfolio-updated/Project6/forcast-calculation.js
@@ -1,34 +1,13 @@
 let x2 = document.querySelector('form')
 
 
-// so it will look for all child element of form and once submit is triggered , then it will pass it s an object
- x2.addEventListener('submit',e =>
-{
-    console.log(e)
-// HERE NAME is datainput // only name of <input get passed with value in JS 
-    let cityname= x2.datainput.value.trim();
-    console.log(cityname)
-    e.preventDefault();
-    x2.reset();
-    // then we call the cityid function with the cituyname
-    wetherinfo(cityname).then(data=>{
-
-    //    let finalcity=data.usercityinfo
-    //     let finaldata= data.citycondition
-updateUI(data)
-
-
-    });
-
-    // now we need to get the id against the cityname that we got form input-data
-
-}
-)
 
 
 wetherinfo= async (cityname)=>{
 // here cityname will be as promise
-let cityname1=await  cityid(cityname);
+
+console.log(cityname)
+const cityname1= await  cityid(cityname);
 // console.log(cityname1.Key)
  console.log(cityname1)
 
@@ -47,9 +26,9 @@ return {usercityinfo:cityname1, citycondition:citydata }
 cityid= async (cityname)=>{
     console.log(cityname)
 /// to fetch location key/id
-    cityid=await fetch( `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=8aZI4psZO7TTBsN4WuFV9mUFyGbjHsAN&q=${cityname}`)
+    cityidx=await fetch( `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=8aZI4psZO7TTBsN4WuFV9mUFyGbjHsAN&q=${cityname}`)
    
-    cityid2= await cityid.json()
+    cityid2= await cityidx.json()
     city3= cityid2[0]
     console.log(city3)
     //  RETURN CITY NAME/KEYCODE
@@ -97,3 +76,27 @@ return citywether2;
 
 
 
+
+// so it will look for all child element of form and once submit is triggered , then it will pass it s an object
+x2.addEventListener('submit',e =>
+{
+    console.log(e)
+// HERE NAME is datainput // only name of <input get passed with value in JS 
+    let cityname= x2.datainput.value.trim();
+    console.log(cityname)
+    e.preventDefault();
+    x2.reset();
+    // then we call the cityid function with the cituyname
+   
+    wetherinfo(cityname).then(data=>{
+
+        //    let finalcity=data.usercityinfo
+        //     let finaldata= data.citycondition
+    updateUI(data)
+    
+    
+        });
+    // now we need to get the id against the cityname that we got form input-data
+
+}
+)
