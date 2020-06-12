3dc3c1d0a6306a683b360230db01d856422fc2d0
rock-007 <59394025+rock-007@users.noreply.github.com>
2020-06-13 00:39:31 +0100

    commit

---
+++ Data Structures/Graphs.js
@@ -0,0 +1,8 @@
+// vertex is a node
+
+// edge is the   connection from one virtex to another
+// undirected graph are having no end node and atleast two edges for a node
+// directoin graph show polarity i.e direction from a to b or c to d  
+// weighted graph is having values with edages
+// Adjacency matrix and Adjacency List to store Graph data
+
