f3bc8dcf3938e5f54440f0995aa05b16e3b48554
rock-007 <59394025+rock-007@users.noreply.github.com>
2020-06-13 01:51:36 +0100

    commit

---
+++ ecoom-app/src/Avatar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Avatar = () => {
+  return (
+    <div className="avatarstyle">
+      <img src="https://joeschmoe.io/api/v1/john" alt="Avatar"></img>
+      <h1>Its Umair Avatar</h1>
+      <p> Developer</p>
+    </div>
+  );
+};
+
+export default Avatar;
