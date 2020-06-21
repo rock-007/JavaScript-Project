c4f4bf3a828e0c851d9a9a4cf3542b6c707e54bf
rock-007 <59394025+rock-007@users.noreply.github.com>
2020-06-22 00:03:52 +0100

    commit

--- ec-server/server.js
+++ ec-server/server.js
@@ -26,6 +26,7 @@ app.get("/api/customers", (req, res) => {
   // });
   let q = "SELECT * from users";
   connection.query(q, function (err, results) {
+    if (err) throw err;
     res.json(results);
   });
 });
