70f55dec89b96e7932c576b4586caa61e061a5fd
rock-007 <59394025+rock-007@users.noreply.github.com>
2020-07-19 17:13:57 +0100

    commit

--- ec-client/src/Component/HomeDisplay.js
+++ ec-client/src/Component/HomeDisplay.js
@@ -5,41 +5,24 @@ import ProjectLogo from "../Img/logo.svg";
 
 // import Button from 'react-bootstrap/Button'; this will only  bring the button module and not whole library if bootstrap
 
-import {
-  Button,
-  ButtonGroup,
-  ToggleButtonGroup,
-  ToggleButton,
-  Dropdown,
-  MenuItem,
-  Container,
-} from "react-bootstrap";
-
-
-
+import { Button, ButtonGroup, ToggleButtonGroup, ToggleButton, Dropdown, MenuItem, Container } from "react-bootstrap";
 
 // props here are arrays of object
 function Home({ props, addBasketitems }) {
-
-  const initialQuantities = props.reduce((quantities, product) => ({ ...quantities, [product.product_name]: 0 }), {})
-  console.log(props)
-  console.log(initialQuantities)
-
+  const initialQuantities = props.reduce((quantities, product) => ({ ...quantities, [product.product_name]: 0 }), {});
+  console.log(props);
+  console.log(initialQuantities);
 
   // for (let i = 0; i < props.length; i++) {
 
-
   //   products[i] = { name:props[i].product_name }
-
 
   // }
 
   // const initialQuantitiy = props.reduce((quantities, product) => ({ ...quantities, [props.product_name]: 0 }, {})) // array of object return
 
-
   // const [quantitiyselected, setQuantity] = useState(initialQuantitiy);
   //quantitiyselected=[{object1},{props.product_name:..},{},...]
-
 
   //   const [yogaMatState, setYogaMatState] = useState("---Select Yogamats---");
   //   const [yogaEquipState, setYogaEquipState] = useState("---Select Equipments---");
@@ -70,33 +53,25 @@ function Home({ props, addBasketitems }) {
   //   } else;
   // };
 
+  const [quantities, setQuantites] = useState(initialQuantities);
 
-
-
-  const [quantities, setQuantites] = useState(initialQuantities)
-
-
-
-
+  const clearselection = (productName) => {
+    setQuantites({ ...quantities, [productName]: 0 });
+  };
 
   const increase = (productName) => {
-    console.log(initialQuantities)
+    console.log(initialQuantities);
     // below o/p
     //  { Yogamatters Eco Everyday Rise Yoga Mat: 0, Yogamatters Organic Cotton Yoga Mat: 0, CorkYogis Classic Cork Yoga Mat: 0, Jade Yoga Voyager Mat: 0 }
     setQuantites({ ...quantities, [productName]: quantities[productName] + 1 });
-    console.log(quantities)
+    console.log(quantities);
     // below o/p
-
-  }
-  console.log(quantities)
-
-
+  };
+  console.log(quantities);
 
   const decrease = (productName) => {
-
     setQuantites({ ...quantities, [productName]: Math.max(0, quantities[productName] - 1) });
-
-  }
+  };
 
   return (
     <div className="products">
@@ -109,7 +84,7 @@ function Home({ props, addBasketitems }) {
         let stockQuantity = eachproduct.stockQuantity;
         return (
           <div className="products" key={producNumber}>
-            <ul >
+            <ul>
               <li>
                 <img className="products-image" src={photo} />
               </li>
@@ -121,26 +96,30 @@ function Home({ props, addBasketitems }) {
               <li>{desc}</li>
               <li>
                 <ButtonGroup aria-label="quantityofproduct">
-                  <Button variant="secondary" name="subtract" value="subtract" onClick={() => decrease(productName)}
-                  >
+                  <Button variant="secondary" name="subtract" value="subtract" onClick={() => decrease(productName)}>
                     -
                   </Button>
                   <Button name={productName} variant="secondary">
                     {quantities[productName]}
                   </Button>
-                  <Button variant="secondary" name="add" value="add" onClick={() => increase(productName)}
-                  >
+                  <Button variant="secondary" name="add" value="add" onClick={() => increase(productName)}>
                     +
                   </Button>
                 </ButtonGroup>
                 &nbsp;
                 {/* will get the value and object passed as on click for all the info of the selectede item */}
-                <Button name={producNumber} value={quantities[productName]} variant="primary"
+                <Button
+                  name={producNumber}
+                  value={quantities[productName]}
+                  variant="primary"
                   onClick={() => {
-                    addBasketitems(eachproduct, quantities[productName])
-                  }}>
+                    addBasketitems(eachproduct, quantities[productName]);
+                    clearselection(productName);
+                    alert(`${productName}, with quantitiy${quantities[productName]} is added to the basket`);
+                  }}
+                >
                   Add to Basket
-                  </Button >
+                </Button>
               </li>
             </ul>
           </div>
@@ -151,6 +130,3 @@ function Home({ props, addBasketitems }) {
 }
 
 export default Home;
-
-
-
