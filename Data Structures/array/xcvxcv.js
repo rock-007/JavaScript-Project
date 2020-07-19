81b55d36a3877ff0a0cc504f58d6d3d0935b2a6a
rock-007 <59394025+rock-007@users.noreply.github.com>
2020-07-19 12:52:46 +0100

    moved

--- Data Structures/array/xcvxcv.js
+++
@@ -1,8 +0,0 @@
-var items = ['mum', 'dad', 'brother'];
-let y= items.filter(function(item) {
-  return item.split('').reverse().join('') === item;
-});
-
-
-
-console.log(y)
--- Data Structures/tempCodeRunnerFile.js
+++
@@ -1,2 +0,0 @@
- else if (a[j]< b[j] && a[j]!= b[j]) bn += bn;
-        //     else if (a[j]= b[j] ) ;
