666eefd79c50adbe3ca19bd6f2d156c4e205439f
rock-007 <59394025+rock-007@users.noreply.github.com>
2020-08-18 16:16:28 +0100

    first commit

--- ec-server/server.js
+++ ec-server/server.js
@@ -372,5 +372,5 @@ app.post("/api/newuser", (req, res) => {
 });
 
 //connection.end();
-const port = 5000;
-app.listen(port, "localhost", "localhost", () => console.log(`server started on port${port}`));
+const port = process.env.PORT || 5000;
+app.listen(port, "localhost", "localhost", () => console.log("port number",`server started on port${port}`));
