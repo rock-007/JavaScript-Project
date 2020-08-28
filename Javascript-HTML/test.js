ffde6417f003193d3a755bd287b0cf641963829f
rock-007 <59394025+rock-007@users.noreply.github.com>
2020-08-28 16:39:07 +0100

    commit

--- Javascript-HTML/test.js
+++ Javascript-HTML/test.js
@@ -14,3 +14,36 @@ const array12 = [1, 2, 3, 4];
 const reducer2 = (accumulator, currentValue) => accumulator + currentValue;
 
 console.log(array12.reduce(reducer2, 0) / array12.length);
+
+
+
+////////////////////?
+
+
+
+
+
+
+
+
+
+
+
+ array41.reduce(addMultiplyAverageArrayValues,{
+sum:0,
+product:1,
+average:0
+}
+Function addMultiplyAverageArrayValues(accumulator,currentvalue,index){
+If(index === array1.length-1){
+Average= accumulator.average/array1.length
+}else Average= accumulator.average+currentvalue
+
+
+abc={
+Sum :accumulator.sum+currentvalue,
+product ;accumulator.product*currentvalue,
+Average: Average;
+
+}
+Return abc
